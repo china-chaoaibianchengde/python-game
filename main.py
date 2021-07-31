b4a8057805bfc1338239703dfe7a10574fbb13d3
洛谷超爱编程的王__ <85790446+china-chaoaibianchengde@users.noreply.github.com>
2021-07-31 11:02:18 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello")
+i = input("please chose language:chinese or english")
+if i == 'chinese':
+  startgame(c);
+  
+elif i == 'english':
+  startgame(e);
+  
+  
+else:
+  print("please chose in chinese or english!")
